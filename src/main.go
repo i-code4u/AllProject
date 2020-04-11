ab18bd88510b107ed785231b225efc212523fae5
shubhamgit4u <shubham.b0507@gmail.com>
2020-04-11 21:26:38 +0530

    registration and login page

--- src/main.go
+++ src/main.go
@@ -15,24 +15,40 @@ func init() {
 	tpl = template.Must(template.ParseGlob("../views/*"))
 }
 func main() {
+
 	mux := httprouter.New()
 	mux.GET("/", index)
 	mux.GET("/login", login)
 	mux.POST("/login", login)
+	mux.POST("/passdata", passdata)
 	mux.ServeFiles("/public/*filepath", http.Dir("../public/"))
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	r.ParseForm()
-	err := tpl.ExecuteTemplate(w, "login.html", 3)
+	err := tpl.ExecuteTemplate(w, "login.html", nil)
 	if err != nil {
 		fmt.Println("error in login")
 	}
 }
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	r.ParseForm()
-	err := tpl.ExecuteTemplate(w, "index.html", 3)
+	err := tpl.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
 		fmt.Println("error in index")
+	}
+}
+func passdata(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	r.ParseForm()
+	type newreg struct {
+		Name     string
+		Email    string
+		Password string
+		Contact  string
+	}
+	dt1 := newreg{r.Form.Get("name"), r.Form.Get("email"), r.Form.Get("password"), r.Form.Get("contact")}
+	err := tpl.ExecuteTemplate(w, "passdata.html", dt1)
+	if err != nil {
+		fmt.Println("error in index", err)
 	}
 }
